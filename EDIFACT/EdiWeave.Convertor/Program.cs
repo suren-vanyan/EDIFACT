2aae456a3e2f18fde31cfaf40a9e71dd29f55c39
Suren Vanyan <Suren.Vanyan@volo.global>
2020-12-04 23:41:03 +0400

    update IFTMBC

--- EDIFACT/EdiWeave.Convertor/Program.cs
+++ EDIFACT/EdiWeave.Convertor/Program.cs
@@ -1,5 +1,4 @@
 ﻿using EdiWeave.Core.Model.Edi;
-using EdiWeave.Edifact.Templates.D99B;
 using EdiWeave.Framework.Readers;
 using Newtonsoft.Json;
 using System.Collections.Generic;
@@ -14,7 +13,7 @@ namespace EdiWeave.Convertor
         static void Main(string[] args)
         {
             //Edifact_INVOIC_D00A    Edifact_IFTMBC_D98B  Invoices_INVOIC
-            const string path = @"C:\Users\suren.vanyan\source\repos\suren-vanyan\EDIFACT\EdiWeave.Convertor\Edifact_IFTMBC_D98B.txt";
+            const string path = @"C:\Users\suren.vanyan\source\repos\EDIFACT\EDIFACT\EdiWeave.Convertor\Edifact_IFTMBC_D98B.txt";
 
             var sample = File.ReadAllText(path);
 
@@ -22,16 +21,16 @@ namespace EdiWeave.Convertor
 
             var ediStream = CommonHelper.GenerateStreamFromString(sample);
 
-            var assembley = Assembly.Load(new AssemblyName("EdiWeave.Edifact.Templates.D99B"));
+            var assembley = Assembly.Load(new AssemblyName("EdiWeave.Edifact.UN.D99B"));
             
 
             // ACT Rules.Edifact   EdiFabric.Templates.Edifact
-            using (var ediReader = new EdifactReader(ediStream, "EdiWeave.Edifact.Templates.D99B"))
+            using (var ediReader = new EdifactReader(ediStream, "EdiWeave.Edifact.UN.D99B"))
             {
                 ediItems = ediReader.ReadToEnd().ToList();
             }
 
-           var models= ediItems.OfType<TSIFTMBC>().ToList();
+           var models= ediItems.OfType<EdiWeave.Edifact.UN.D99B.TSIFTMBC>().ToList();
             var model = JsonConvert.SerializeObject(models[0], new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
 
         }
